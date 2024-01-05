a2d6e04ef2e7ee979b42719e07751a04b6c17099
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-01-05 09:09:52 -0500

    delete files

--- src/app/home/(sections)/storeSection.tsx
+++ src/app/home/(sections)/storeSection.tsx
@@ -3,11 +3,11 @@ import { Button, Image } from "@nextui-org/react";
 export const StoreSection = () => {
   return (
     <section className="">
-      <div className="grid grid-cols-2 items-center bg-black h-72 m-14">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center bg-black h-72 sm:m-14">
         <div>
           {/* <Image /> */}
         </div>
-        <div className="h-fit space-y-4">
+        <div className="h-fit space-y-4 p-4 sm:p-6">
           <h1 className="text-2xl font-bold text-white">
             PROXIMAMENTE UBICA TU TIENDA FAVORITA
           </h1>
