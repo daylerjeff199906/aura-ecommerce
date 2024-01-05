6e7e0b707deaae604d2884cb1befa1a9f1cb7c40
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-01-05 18:19:41 -0500

    fixe page products

--- src/app/(client)/productos/(sections)/FiltersSection.tsx
+++ src/app/(client)/productos/(sections)/FiltersSection.tsx
@@ -1,5 +1,30 @@
+import { Accordion, AccordionItem } from "@nextui-org/react";
+
 export const FiltersSection = () => {
   return (
-    <aside className="hidden lg:block lg:max-w-[14rem] xl:max-w-[16rem] w-full sm:sticky sm:h-screen  sm:top-48"></aside>
+    <aside className="hidden lg:block lg:max-w-[14rem] xl:max-w-[16rem] w-full sm:sticky sm:h-screen  sm:top-48">
+      <Accordion>
+        <AccordionItem title="Categorías">
+          <div className="flex flex-col">
+            <div className="flex items-center space-x-2">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Categoría 1</label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Categoría 2</label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Categoría 3</label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Categoría 4</label>
+            </div>
+          </div>
+        </AccordionItem>
+      </Accordion>
+    </aside>
   );
 };
