6bccdcf74d1d91cf38495b89afb4cb72a26f76d0
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-01-07 11:03:08 -0500

    resolved conflicts

---
+++ src/app/(client)/carrito/(sections)/detailsSection.tsx
@@ -0,0 +1,10 @@
+export const DetailsSection = () => {
+  return (
+    <section className="container">
+      <div className="pt-12 text-center">
+        <h1 className="text-3xl font-medium">Shopping Cart</h1>
+        <h3 className="text-sm text-pink-500">Shop</h3>
+      </div>
+    </section>
+  );
+};
