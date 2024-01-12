19cf8c31cfb124d3d86b0e84e4ef7c42f5ee6a56
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-01-12 17:12:18 -0500

    fixed crud products

--- src/app/(admin)/admin/productos/[slug]/(section)/frmProduct.tsx
+++ src/app/(admin)/admin/productos/[slug]/(section)/frmProduct.tsx
@@ -20,6 +20,8 @@ export const FrmProduct = () => {
     createdAt: new Date(),
   });
 
+  const radius = "sm";
+
   const handleAddProduct = () => {
     addProduct(product);
   };
@@ -48,12 +50,14 @@ export const FrmProduct = () => {
     <>
       <div className="space-y-4">
         <Input
+          radius={radius}
           label="Nombre"
           placeholder="Nombre"
           value={product.name}
           onChange={(e) => setProduct({ ...product, name: e.target.value })}
         />
         <Textarea
+          radius={radius}
           label="Descripción"
           placeholder="Descripción"
           value={product.description}
@@ -61,7 +65,13 @@ export const FrmProduct = () => {
             setProduct({ ...product, description: e.target.value })
           }
         />
-        <Select label="Categoría" placeholder="Categoría">
+        <Select
+          label="Categoría"
+          placeholder="Categoría"
+          radius={radius}
+          selectedKeys={[product.category]}
+          onChange={(e) => setProduct({ ...product, category: e.target.value })}
+        >
           {categories ? (
             categories?.map((category) => (
               <SelectItem key={category?.value} value={category?.value}>
@@ -75,33 +85,40 @@ export const FrmProduct = () => {
           )}
         </Select>
 
-        <Input
-          label="Precio"
-          placeholder="Precio"
-          value={product.price}
-          onChange={(e) => setProduct({ ...product, price: e.target.value })}
-        />
-        <Input
-          label="Stock"
-          placeholder="Stock"
-          value={product.stock.toString()}
-          onChange={(e) =>
-            setProduct({ ...product, stock: parseInt(e.target.value) })
-          }
-        />
-        <Input
-          label="discount"
-          placeholder="discount"
-          value={product.discount.toString()}
-          onChange={(e) =>
-            setProduct({ ...product, discount: parseInt(e.target.value) })
-          }
-        />
+        <div className="flex gap-4">
+          <Input
+            radius={radius}
+            label="Precio"
+            placeholder="Precio"
+            value={product.price}
+            onChange={(e) => setProduct({ ...product, price: e.target.value })}
+          />
+          <Input
+            radius={radius}
+            label="Stock"
+            placeholder="Stock"
+            type="number"
+            min={0}
+            value={product.stock.toString()}
+            onChange={(e) =>
+              setProduct({ ...product, stock: parseInt(e.target.value) })
+            }
+          />
+          <Input
+            radius={radius}
+            label="Discount"
+            placeholder="discount"
+            value={product.discount.toString()}
+            onChange={(e) =>
+              setProduct({ ...product, discount: parseInt(e.target.value) })
+            }
+          />
+        </div>
         <div className="flex gap-2">
-          <Button color="primary" onClick={handleAddProduct}>
+          <Button color="primary" onClick={handleAddProduct} radius={radius}>
             Guardar
           </Button>
-          <Button color="danger" onClick={handleClearProduct}>
+          <Button color="danger" onClick={handleClearProduct} radius={radius}>
             Cancelar
           </Button>
         </div>
