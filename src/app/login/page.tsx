572527d14431e4ea243369ce0c48637ab229ab02
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-01-12 09:13:14 -0500

    fixed login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,41 @@
-export default function PageLogin() {
-  return <></>;
+"use client";
+import { useState } from "react";
+import auth from "@/firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      // Iniciar sesión con Firebase Auth
+      // Puedes usar también createUserWithEmailAndPassword para el registro
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Inicio de sesión exitoso");
+    } catch (error) {
+      console.error("Error al iniciar sesión", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Página de inicio de sesión</h1>
+      <div className="flex flex-row gap-4">
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Correo electrónico"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Contraseña"
+        />
+        <button onClick={handleLogin}>Iniciar sesión</button>
+      </div>
+    </div>
+  );
 }
