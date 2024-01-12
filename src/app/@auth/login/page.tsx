fdb451c5c0c03358511aea85b73cb29dbf53f25a
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-01-12 09:11:33 -0500

    fixed auth

--- src/app/@auth/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    // <Modal>
-    <h1>Login</h1>
-    // </Modal>
-  );
-}
--- src/providers/admin/authProvider.tsx
+++ src/providers/admin/authProvider.tsx
@@ -1,21 +1,20 @@
-// PrivateRoute.tsx
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
-// import auth from "./auth"; // Importa tu módulo auth
+import auth from "@/firebase/auth";
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
-  //   useEffect(() => {
-  //     const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //       if (!user) {
-  //         router.push("/login"); // Redirige a la página de inicio de sesión si el usuario no está autenticado
-  //       }
-  //     });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push("/login"); // Redirige a la página de inicio de sesión si el usuario no está autenticado
+      }
+    });
 
-  //     return () => unsubscribe();
-  //   }, [router]);
+    return () => unsubscribe();
+  }, [router]);
 
   return <>{children}</>;
 };
