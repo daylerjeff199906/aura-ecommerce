e9ad5ecdced667caa4a579ceea23ef897bbd51fe
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-01-14 08:42:34 -0500

    add files

---
+++ src/app/(admin)/admin/sliders/page.tsx
@@ -0,0 +1,11 @@
+import { ListSection } from "./(sections)/listSection";
+export default function PageSliders() {
+  return (
+    <>
+      <h1 className="text-xl font-medium">Sliders</h1>
+      <section className="py-6">
+        <ListSection />
+      </section>
+    </>
+  );
+}
